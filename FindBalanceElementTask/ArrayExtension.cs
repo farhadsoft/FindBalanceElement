e61420cd4e4bd49910b5c1d46c466b00f53e2133
Farhad <epam@farhad.su>
2021-01-04 13:23:37 +0300

    Done!

--- FindBalanceElementTask/ArrayExtension.cs
+++ FindBalanceElementTask/ArrayExtension.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 
 namespace FindBalanceElementTask
 {
@@ -17,7 +17,40 @@ namespace FindBalanceElementTask
         /// <exception cref="ArgumentException">Thrown when source array is empty.</exception>
         public static int? FindBalanceElement(int[] array)
         {
-            throw new NotImplementedException("You need to implement this method.");
+            if (array is null)
+            {
+                throw new ArgumentNullException($"Thrown when source array is null. {nameof(array)}");
+            }
+
+            if (array.Length == 0)
+            {
+                throw new ArgumentException("Thrown when source array is empty.");
+            }
+
+            int lenght = array.Length;
+            int[] preSum = new int[lenght];
+            preSum[0] = array[0];
+            for (int i = 1; i < lenght; i++)
+            {
+                preSum[i] = preSum[i - 1] + array[i];
+            }
+
+            int[] sufSum = new int[lenght];
+            sufSum[lenght - 1] = array[lenght - 1];
+            for (int i = lenght - 2; i >= 0; i--)
+            {
+                sufSum[i] = sufSum[i + 1] + array[i];
+            }
+
+            for (int i = 1; i < lenght - 1; i++)
+            {
+                if (preSum[i] == sufSum[i])
+                {
+                    return i;
+                }
+            }
+
+            return null;
         }
     }
 }
